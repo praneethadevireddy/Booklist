698bc12f7237e3a79169e7ebe1b377023181f317
praneethadevireddy <68987083+praneethadevireddy@users.noreply.github.com>
2024-07-15 14:34:45 -0500

    Update settings.py

--- bookproject/bookproject/settings.py
+++ bookproject/bookproject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+# BASE_DIR is the directory containing manage.py
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Add this line to specify the directory for collected static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
