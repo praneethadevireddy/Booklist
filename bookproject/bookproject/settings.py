3a135953ecb447a0df38ade33bc310b2d12accd7
praneethadevireddy <68987083+praneethadevireddy@users.noreply.github.com>
2024-07-15 14:28:37 -0500

    Update settings.py

--- bookproject/bookproject/settings.py
+++ bookproject/bookproject/settings.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+# Add this line to specify the directory for collected static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Ensure you have the STATIC_URL setting as well
+STATIC_URL = '/static/'
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
